26825d529fce4b841d04093e2664d5b092de830c
llavina <bakuko29@gmail.com>
2014-06-19 16:46:06 +0800

    changes made on vless.sh since the "vimcurrent" has been removed so need to specify the exact vim version

--- vless/vless.sh
+++ vless/vless.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # colorful! :)
 
-vim="`which vim`"
-path="/usr/share/vim/vimcurrent/macros/less.sh"
+vim_ver="`/usr/bin/vim --version | grep "Vi IMproved" | awk '{print $5}' | sed -e 's/\.//g'`"
+path="/usr/share/vim/vim$vim_ver/macros/less.sh"
 
-if [ -z $vim ]; then
+#echo $path
+
+
+if [ -z $vim_ver ]; then
    echo "Install vim first"
 else
    ln -ns $path /usr/bin/vless
 fi
 
-#luilav
+echo "How to use? just \"vless filename\"" 
+echo " Enjoy!"
+
+## @chojayr
 
